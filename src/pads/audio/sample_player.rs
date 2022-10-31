bb7e4352d6e7caaa1d45c28edba70846368ef4c3
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2022-10-31 13:50:21 +0300

    Remove useless commented code

--- src/pads/audio/sample_player.rs
+++ src/pads/audio/sample_player.rs
@@ -1,7 +1,6 @@
 use rodio::{Decoder, OutputStream, Source, OutputStreamHandle};
 use std::io::BufReader;
 use std::fs::File;
-use crate::pads::audio::sample::Sample;
 
 pub struct SamplePlayer {
     output_handle: OutputStreamHandle,
@@ -18,20 +17,4 @@ impl SamplePlayer {
         let decoder = Decoder::new(BufReader::new(file)).unwrap();
         &self.output_handle.play_raw(decoder.convert_samples());
     }
-}
-
-// pub fn play_sample(filename: String) {
-//     // Do nothing if pad has no audio file inside.
-
-//     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-
-//     let file = BufReader::new(File::open(filename).unwrap());
-//     let source = Decoder::new(file).unwrap();
-
-//     stream_handle.play_raw(source.convert_samples()).expect("Failed to play samples.");
-// }
-
-pub fn load_test_samples(test_sample: &str) -> Sample {
-    let file = File::open(test_sample).unwrap();
-    Sample::new(file)
 }
